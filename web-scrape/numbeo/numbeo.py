7ffdbd1ecf2f84e9d5daff4c25c725b0044fc19d
Kliative <kliation@gmail.com>
2019-01-28 16:56:14 +0200

    Q-3-Web-Scraper  Numbeo Scrappy

--- web-scrape/numbeo/numbeo.py
+++ web-scrape/numbeo/numbeo.py
@@ -10,6 +10,5 @@ class NumbeoSpider(scrapy.Spider):
     def parse(self, response):
         self.log('I just visited: ' + response.url)
         yield {
-            'product_name': response.css('td.tr_highlighted::text').extract_first(),
-            'price': response.css('td.priceValue::text').extract_first()
+            'product_name': response.css('.barTextLeft::text').extract_first()
         }
